5bab98fee835873a03a17750678355006c93ef1c
Everardo Rosales <erosales@mit.edu>
2015-04-24 13:39:02 -0400

    Fixed slider.

--- static/js/index.js
+++ static/js/index.js
@@ -140,23 +140,23 @@ $(window).load(function() {
 
 $(function(){
   $('#slider').slider({
-    value:-1,
-    min:-1,
-    max:2,
+    value:10,
+    min:1,
+    max:100,
     step:1,
     slide: function(event, ui){
-      if(ui.value == -1 || ui.value == 0){
-        $('#dist').val("< "+Math.pow(10,ui.value)+ " mile");
+      if(ui.value == 1){
+        $('#dist').val("< "+ ui.value + " mile");
 
       }else{
-        $('#dist').val("< "+Math.pow(10,ui.value) + " miles");
+        $('#dist').val("< "+ ui.value + " miles");
       }
     },
     change: function(event, ui){}
   });
-  var initDist = "< "+Math.pow(10,$('#slider').slider("value")) + " mile";
+  var initDist = "< "+ $('#slider').slider("value") + " mile";
   var initDistDiv = "<div class='param'>"+initDist+"</div>";
-  $('#dist').val("< "+Math.pow(10,$('#slider').slider("value")) + " mile");
+  $('#dist').val("< "+ $('#slider').slider("value") + " mile");
   $('#searchParams').html(initDistDiv);
 });
 $(function(){
