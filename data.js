07d37f7f266a6b2e21850ee6ef8bc596af7f6afa
Lasya <lasya@mit.edu>
2015-04-27 16:34:24 -0400

    added mustache template for listings

---
+++ data.js
@@ -0,0 +1,170 @@
+listingsData = {
+  "petListings": [{
+    ownerName: "Cornelio Rosales",
+    petName: "Fluffy",
+    petType: "Snake",
+    petImg: "fluffy0.jpg",
+    zipcode: 02142,
+    startDate: "4/20/2015",
+    endDate: "5/1/2015",
+    payment: "$"
+  }, {
+    ownerName: "Lily Paxton",
+    petName: "Sugar",
+    petType: "Dog",
+    petImg: "dog-beach.jpg",
+    zipcode: 02139,
+    startDate: "6/15/2015",
+    endDate: "6/17/2015",
+    payment: "$$"
+  }, {
+    ownerName: "Janet Mason",
+    petName: "Burrito",
+    petType: "Cat",
+    petImg: "burritoCat0.jpg",
+    zipcode: 02142,
+    startDate: "5/20/2015",
+    endDate: "5/21/2015", 
+    payment: "$$$"
+  }, {
+    ownerName: "Penny Marshall",
+    petName: "Okabe Rintaro",
+    petType: "Bird",
+    petImg: "bird0.jpg",
+    zipcode: 02467,
+    startDate: "9/2/2015",
+    endDate: "10/1/2015", 
+    payment: "$$$$"
+  }, {
+    ownerName: "Lin Mei",
+    petName: "Napoleon",
+    petType: "Dog",
+    petImg: "puppy0.jpg",
+    zipcode: 02142,
+    startDate: "4/20/2015",
+    endDate: "5/10/2015",
+    payment: "$"
+  }, {
+    ownerName: "Shaila Higgins",
+    petName: "Genghis Khan",
+    petType: "Hamster",
+    petImg: "hamster1.jpg",
+    zipcode: 02142,
+    startDate: "4/15/2015",
+    endDate: "5/1/2015",
+    payment: "$"
+  }, {
+    ownerName: "Doug Schumaker",
+    petName: "Porky",
+    petType: "Dog",
+    petImg: "puppy1.jpg",
+    zipcode: 02142,
+    startDate: "6/14/2015",
+    endDate: "6/21/2015",
+    payment: "$$"
+  }, {
+    ownerName: "Minnie Preston",
+    petName: "Snoop Dogg",
+    petType: "Cat",
+    petImg: "cat0.jpg",
+    zipcode: 02459,
+    startDate: "8/20/2015",
+    endDate: "9/10/2015",
+    payment: "$$$$"
+  }, {
+    ownerName: "Flynn Rider",
+    petName: "Sam the Wise",
+    petType: "Rabbit",
+    petImg: "bunny0.jpg",
+    zipcode: 02142,
+    startDate: "4/21/2015",
+    endDate: "4/28/2015",
+    payment: "$"
+  }, {
+    ownerName: "Craig Newton",
+    petName: "Miss Piggy",
+    petType: "Guinea Pig",
+    petImg: "guineaPig0.jpg",
+    zipcode: 02142,
+    startDate: "7/17/2015",
+    endDate: "8/1/2015",
+    payment: "$$"
+  }, {
+    ownerName: "Cornelia Rose",
+    petName: "Rapunzel",
+    petType: "Dog",
+    petImg: "dog0.jpg",
+    zipcode: 02142,
+    startDate: "9/20/2015",
+    endDate: "11/1/2015",
+    payment: "$$"
+  }, {
+    ownerName: "Laura Pine",
+    petName: "Granny Laura",
+    petType: "Cat",
+    petImg: "cat1.jpg",
+    zipcode: 02142,
+    startDate: "4/20/2015",
+    endDate: "5/1/2015",
+    payment: "$"
+  }, {
+    ownerName: "Stephanie Castle",
+    petName: "Pumpkin",
+    petType: "Hamster",
+    petImg: "hamster0.jpg",
+    zipcode: 02139,
+    startDate: "5/1/2015",
+    endDate: "5/2/2015",
+    payment: "$"
+  }, {
+    ownerName: "Deng Xiaoping",
+    petName: "Mao Zedong",
+    petType: "Cat",
+    petImg: "chinaCat.jpg",
+    zipcode: 02142,
+    startDate: "4/20/2015",
+    endDate: "5/1/2015",
+    payment: "Negotiable"
+  }],
+  "personListings": [{
+    sitterName: "Lin Mei",
+    sitterImg: "LinMei.jpg",
+    zipcode: 02142,
+    petTypes: [{type: "Dog"}, {type: "Cat"}]
+  }, {
+    sitterName: "Penny Marshall",
+    sitterImg: "pennyMarshall.jpg",
+    zipcode: 02139,
+    petTypes: [{type: "Dog"}]
+  }, {
+    sitterName: "Stephanie Castle",
+    sitterImg: "stephanieCastle.jpg",
+    zipcode: 02142,
+    petTypes: [{type: "Dog"}, {type: "Cat"}, {type: "Hamster"}]
+  }, {
+    sitterName: "Craig Newton",
+    sitterImg: "craigNewton.jpg",
+    zipcode: 02142,
+    petTypes: [{type: "Dog"}]
+  }, {
+    sitterName: "Janet Mason",
+    sitterImg: "JanetMason.jpg",
+    zipcode: 02459,
+    petTypes: [{type: "Cat"}, {type: "Hamster"}, {type: "Guinea Pig"}]
+  }, {
+    sitterName: "Flynn Rider",
+    sitterImg: "FlynnRider.jpeg",
+    zipcode: 02142,
+    petTypes: [{type: "Dog"}, {type: "Cat"}, {type: "Snake"}]
+  }, {
+    sitterName: "Missy Clinton",
+    sitterImg: "MissyClinton.jpg",
+    zipcode: 02139,
+    petTypes: [{type: "Cat"}]
+  }, {
+    sitterName: "Doug Schumaker",
+    sitterImg: "DougSchumaker.jpg",
+    zipcode: 02214,
+    petTypes: [{type: "Cat"}, {type: "Rabbit"}]
+  }]
+}
