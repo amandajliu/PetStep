c5a8d49f681dbc5bcbc773800129a93b6263d0a1
Lasya <lasya@mit.edu>
2015-04-29 19:43:53 -0400

    added profiles for lily and flynn

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ listingsData = {
     petName: "Fluffy",
     petType: "Snake",
     petImg: "fluffy0.jpg",
-    zipcode: 02142,
+    zipcode: "02142",
     startDate: "4/20/2015",
     endDate: "5/1/2015",
     payment: "$"
@@ -13,7 +13,7 @@ listingsData = {
     petName: "Sugar",
     petType: "Dog",
     petImg: "dog-beach.jpg",
-    zipcode: 02139,
+    zipcode: "02139",
     startDate: "6/15/2015",
     endDate: "6/17/2015",
     payment: "$$"
@@ -22,7 +22,7 @@ listingsData = {
     petName: "Burrito",
     petType: "Cat",
     petImg: "burritoCat0.jpg",
-    zipcode: 02142,
+    zipcode: "02142",
     startDate: "5/20/2015",
     endDate: "5/21/2015", 
     payment: "$$$"
@@ -31,7 +31,7 @@ listingsData = {
     petName: "Okabe Rintaro",
     petType: "Bird",
     petImg: "bird0.jpg",
-    zipcode: 02467,
+    zipcode: "02467",
     startDate: "9/2/2015",
     endDate: "10/1/2015", 
     payment: "$$$$"
@@ -40,7 +40,7 @@ listingsData = {
     petName: "Napoleon",
     petType: "Dog",
     petImg: "puppy0.jpg",
-    zipcode: 02142,
+    zipcode: "02142",
     startDate: "4/20/2015",
     endDate: "5/10/2015",
     payment: "$"
@@ -49,7 +49,7 @@ listingsData = {
     petName: "Genghis Khan",
     petType: "Hamster",
     petImg: "hamster1.jpg",
-    zipcode: 02142,
+    zipcode: "02142",
     startDate: "4/15/2015",
     endDate: "5/1/2015",
     payment: "$"
@@ -58,7 +58,7 @@ listingsData = {
     petName: "Porky",
     petType: "Dog",
     petImg: "puppy1.jpg",
-    zipcode: 02142,
+    zipcode: "02142",
     startDate: "6/14/2015",
     endDate: "6/21/2015",
     payment: "$$"
@@ -67,7 +67,7 @@ listingsData = {
     petName: "Snoop Dogg",
     petType: "Cat",
     petImg: "cat0.jpg",
-    zipcode: 02459,
+    zipcode: "02459",
     startDate: "8/20/2015",
     endDate: "9/10/2015",
     payment: "$$$$"
@@ -76,7 +76,7 @@ listingsData = {
     petName: "Sam the Wise",
     petType: "Rabbit",
     petImg: "bunny0.jpg",
-    zipcode: 02142,
+    zipcode: "02142",
     startDate: "4/21/2015",
     endDate: "4/28/2015",
     payment: "$"
@@ -85,7 +85,7 @@ listingsData = {
     petName: "Miss Piggy",
     petType: "Guinea Pig",
     petImg: "guineaPig0.jpg",
-    zipcode: 02142,
+    zipcode: "02142",
     startDate: "7/17/2015",
     endDate: "8/1/2015",
     payment: "$$"
@@ -94,7 +94,7 @@ listingsData = {
     petName: "Rapunzel",
     petType: "Dog",
     petImg: "dog0.jpg",
-    zipcode: 02142,
+    zipcode: "02142",
     startDate: "9/20/2015",
     endDate: "11/1/2015",
     payment: "$$"
@@ -103,7 +103,7 @@ listingsData = {
     petName: "Granny Laura",
     petType: "Cat",
     petImg: "cat1.jpg",
-    zipcode: 02142,
+    zipcode: "02142",
     startDate: "4/20/2015",
     endDate: "5/1/2015",
     payment: "$"
@@ -112,7 +112,7 @@ listingsData = {
     petName: "Pumpkin",
     petType: "Hamster",
     petImg: "hamster0.jpg",
-    zipcode: 02139,
+    zipcode: "02139",
     startDate: "5/1/2015",
     endDate: "5/2/2015",
     payment: "$"
@@ -121,7 +121,7 @@ listingsData = {
     petName: "Mao Zedong",
     petType: "Cat",
     petImg: "chinaCat.jpg",
-    zipcode: 02142,
+    zipcode: "02142",
     startDate: "4/20/2015",
     endDate: "5/1/2015",
     payment: "Negotiable"
@@ -129,42 +129,108 @@ listingsData = {
   "personListings": [{
     sitterName: "Lin Mei",
     sitterImg: "LinMei.jpg",
-    zipcode: 02142,
+    zipcode: "02142",
     petTypes: ["Dog", "Cat"]
   }, {
     sitterName: "Penny Marshall",
     sitterImg: "pennyMarshall.jpg",
-    zipcode: 02139,
+    zipcode: "02139",
     petTypes: ["Dog"]
   }, {
     sitterName: "Stephanie Castle",
     sitterImg: "stephanieCastle.jpg",
-    zipcode: 02142,
+    zipcode: "02142",
     petTypes: ["Dog", "Cat", "Hamster"]
   }, {
     sitterName: "Craig Newton",
     sitterImg: "craigNewton.jpg",
-    zipcode: 02142,
+    zipcode: "02142",
     petTypes: ["Dog"]
   }, {
     sitterName: "Janet Mason",
     sitterImg: "JanetMason.jpg",
-    zipcode: 02459,
+    zipcode: "02459",
     petTypes: ["Cat", "Hamster", "Guinea Pig"]
   }, {
     sitterName: "Flynn Rider",
     sitterImg: "FlynnRider.jpeg",
-    zipcode: 02142,
+    zipcode: "02142",
     petTypes: ["Dog", "Cat", "Snake"]
   }, {
     sitterName: "Missy Clinton",
     sitterImg: "MissyClinton.jpg",
-    zipcode: 02139,
+    zipcode: "02139",
     petTypes: ["Cat"]
   }, {
     sitterName: "Doug Schumaker",
     sitterImg: "DougSchumaker.jpg",
-    zipcode: 02214,
+    zipcode: "02214",
     petTypes: ["Cat", "Rabbit"]
   }]
+}
+
+profileData = {
+    "users" : [{
+        currentUser: true,
+        username: "cornelio",
+        name: "Cornelio Rosales", 
+        userImg: "Cornelio.png", 
+        sitsFor: [],
+        zipcode: "02142",
+        pets: [{
+            petType: "Snake",
+            petName: "Fluffy",
+            petImg: "fluffy0.jpg"
+        }],
+        ownerRating: 4,
+        sitterRating: null,
+        reviews: [{
+            reviewer: "Lily Paxton",
+            owner: "Cornelio",
+            pet: "Fluffy",
+            ownerRating: 3,
+            petRating: 5,
+            message: "Fluffy was awesome but Cornelio was not."
+        }, {
+            reviewer: "Flynn Rider",
+            owner: "Cornelio",
+            pet: "Fluffy",
+            ownerRating: 5,
+            petRating: 5,
+            message: "FLUFFY IS THE BEST SNAKE EVER!!!!!!!!!!!!AND CORNELIO IS MY BEST FRIEND!!!!!!!"
+        }], 
+        bio: "Hi! I'm Cornelio and I have a pet snake named Fluffy. I am looking for professional sitters who have experience with snakes."
+    }, {
+        currentUser: false,
+        username: "lily",
+        name: "Lily Paxton",
+        userImg: "lilyPaxton.jpg",
+        sitsFor: [],
+        zipcode: "02139",
+        pets: [{
+            petType: "Dog",
+            petName: "Sugar",
+            petImg: "dog-beach.jpg"
+        }], 
+        ownerRating: 4,
+        sitterRating: 5,
+        reviews: [],
+        bio: "Lily Paxton's biography"
+    }, {
+        currentUser: false,
+        username: "flynn",
+        name: "Flynn Rider",
+        userImg: "FlynnRider.jpeg",
+        sitsFor: ["Dog", "Cat", "Snake"],
+        zipcode: "02142",
+        pets: [{
+            petType: "Rabbit",
+            petName: "Sam the Wise",
+            petImg: "bunny0.jpg"
+        }], 
+        ownerRating: 3,
+        sitterRating: 4,
+        reviews: [], 
+        bio: "Flynn Rider's biography"
+    }]
 }
